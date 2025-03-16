f833a788cdacafa21cf147d1995bbdda5a705bbb
RiddhiDapke <rbdapke_b22@it.vjti.ac.in>
2025-03-16 10:41:39 +0530

    Initial commit

---
+++ src/main/java/com/wellsfargo/counselor/entity/Client.java
@@ -0,0 +1,36 @@
+package com.wellsfargo.counselor.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Client {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    @ManyToOne
+    @JoinColumn(name = "advisor_id")
+    private FinancialAdvisor advisor;
+
+    @OneToOne(mappedBy = "client", cascade = CascadeType.ALL, orphanRemoval = true)
+    private Portfolio portfolio;
+
+    public Client() {}
+
+    public Client(String name, FinancialAdvisor advisor) {
+        this.name = name;
+        this.advisor = advisor;
+    }
+
+    public Long getId() { return id; }
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
+    public FinancialAdvisor getAdvisor() { return advisor; }
+    public void setAdvisor(FinancialAdvisor advisor) { this.advisor = advisor; }
+    public Portfolio getPortfolio() { return portfolio; }
+    public void setPortfolio(Portfolio portfolio) { this.portfolio = portfolio; }
+}
+
